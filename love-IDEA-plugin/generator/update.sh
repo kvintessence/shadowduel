618ce3731b2ad1f3a01d5a507cef537af7d1f1e6
Denis Sorokin <deniss@zeptolab.com>
2018-01-29 00:05:13 +0300

    don't need this

--- love-IDEA-plugin/generator/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-readonly LOVE_DIR=love_0102
-
-git clone --depth=1 https://github.com/rm-code/love-api api
-mkdir docs
-lua generator.lua
-mv -v docs/* ../$LOVE_DIR/
-rmdir docs
-rm -rf api
